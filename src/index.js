0349cbe51ef2c4f90a15071fc6641c6e4f4067ae
supersharmapunit <punit2000sharma@gmail.com>
2022-12-26 21:24:09 +0530

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
+const db = require('./models/index');
+const apiRoutes = require('./routes/index');
+
+const app = express();
+
+const setupAndStartServer = async () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server started on ${PORT}`);
+        if (process.env.DB_SYNC) {
+            db.sequelize.sync({ alter: true });
+        }
+    });
+};
+
+setupAndStartServer();
