b645565edb872414a1d1afb17c5eee92325538ea
supersharmapunit <50194646+supersharmapunit@users.noreply.github.com>
2023-01-15 20:28:32 +0530

    Added rabbit-mq setup

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const { BookingController } = require('../../controllers/index');
+// const { createChannel } = require('../../utils/messageQueue');
 
 const router = express.Router();
+// const channel = await createChannel();
+const bookingController = new BookingController();
 
-router.post('/bookings', BookingController.create);
+router.post('/bookings', bookingController.create);
+router.post('/publish', bookingController.sendMessageToQueue);
 
 module.exports = router;
